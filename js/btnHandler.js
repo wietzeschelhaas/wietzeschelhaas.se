7bfc214770600629dd45853e1a489b3980d8f24f
wietzeschelhaas <wietze.pc@gmail.com>
2020-10-27 16:41:22 +0100

    added project info

---
+++ js/btnHandler.js
@@ -0,0 +1,37 @@
+$("#loopGooglePlay").click(function (e) {
+    window.open("https://play.google.com/store/apps/details?id=com.wsgames.loop&hl=sv");
+});
+
+
+$("#loopGithub").click(function (e) {
+    window.open("https://github.com/wietzeschelhaas/Loop");
+});
+
+$("#arcGithub").click(function (e) {
+    window.open("https://github.com/wietzeschelhaas/ArcGridParser");
+});
+
+
+$("#startFlappy").click(function (e) {
+    window.open("flappy.html");
+});
+
+$("#flappyGithub").click(function (e) {
+    window.open("https://github.com/wietzeschelhaas/FlappyNeuroEvolution");
+});
+
+$("#webServerGit").click(function (e) {
+    window.open("https://github.com/wietzeschelhaas/webServer");
+});
+
+$("#roboStart").click(function (e) {
+    window.open("robo.html");
+});
+
+$("#gitChess").click(function (e) {
+    window.open("https://github.com/wietzeschelhaas/PyChess");
+});
+
+$("#gitNN").click(function (e) {
+    window.open("https://github.com/wietzeschelhaas/NNFromScratch");
+});
